e98343e871f822a67bae4fca87c0f3d1ae8f040a
Yuriy <germanovich4job@gmail.com>
2026-03-15 02:14:40 +0300

    3

--- .eslintrc.base.js
+++ .eslintrc.base.js
@@ -9,29 +9,23 @@ module.exports = {
     ecmaVersion: 'latest',
     sourceType: 'module',
     ecmaFeatures: {
-      jsx: true, // полезно для React, но не мешает и в NestJS
+      jsx: true,
     },
   },
-  plugins: [
-    '@typescript-eslint',
-    'import',
-    'unused-imports',
-  ],
+  plugins: ['@typescript-eslint', 'import', 'unused-imports'],
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:import/recommended',
     'plugin:import/typescript',
-    'prettier', // отключает конфликтующие с Prettier правила ESLint
+    'prettier',
   ],
   rules: {
-    // Удаление неиспользуемых импортов
     'unused-imports/no-unused-imports': 'error',
     'unused-imports/no-unused-vars': [
       'error',
       { vars: 'all', varsIgnorePattern: '^_', args: 'after-used', argsIgnorePattern: '^_' },
     ],
-    // Сортировка импортов
     'import/order': [
       'error',
       {
@@ -40,7 +34,6 @@ module.exports = {
         alphabetize: { order: 'asc', caseInsensitive: true },
       },
     ],
-    // Отключаем стандартное правило no-unused-vars в пользу плагина
     'no-unused-vars': 'off',
     '@typescript-eslint/no-unused-vars': 'off',
   },
--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,4 @@
 module.exports = {
   root: true,
-  extends: ['./.eslintrc.base.js'], // или просто импорт базового файла
+  extends: ['./.eslintrc.base.js'],
 };

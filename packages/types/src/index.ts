b4273cc279f6a332a4700422c31ed545ca209eb4
Yuriy <germanovich4job@gmail.com>
2026-03-15 22:38:55 +0300

    sfdd

--- packages/types/src/index.ts
+++ packages/types/src/index.ts
@@ -1,1 +1,37 @@
+export * from './client/attachment/dto';
+export * from './client/attachment/entities';
+
+export * from './client/estimate-item/dto';
+export * from './client/estimate-item/entities';
+
+export * from './client/estimate/dto';
+export * from './client/estimate/entities';
+
+export * from './client/message/dto';
+export * from './client/message/entities';
+
+export * from './client/object/dto';
+export * from './client/object/entities';
+
+export * from './client/product/dto';
+export * from './client/product/entities';
+
+export * from './client/product-image/dto';
+export * from './client/product-image/entities';
+
+export * from './client/project/dto';
+export * from './client/project/entities';
+
+export * from './client/project-document/dto';
+export * from './client/project-document/entities';
+
+export * from './client/project-image/dto';
+export * from './client/project-image/entities';
+
+export * from './client/shipment/dto';
+export * from './client/shipment/entities';
+
+export * from './client/shipment-item/dto';
+export * from './client/shipment-item/entities';
+
 export * from './shared/enums';
